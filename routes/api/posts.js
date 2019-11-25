d9e0da23eda3286eeab1a9d92b4bef043637a863
sanket2197 <sanket9721kumar@gmail.com>
2019-11-25 22:44:39 +0530

    File Upload to Specific Directory

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 const Post = require("../../models/Post");
 const multer = require("multer");
+const fileUploadPath = "./client/src/static/uploads";
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, fileUploadPath);
   },
   filename: function(req, file, cb) {
     cb(null, file.originalname);
@@ -64,5 +65,18 @@ router.post(
     }
   }
 );
+
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// app.use("/uploads", express.static("uploads"));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 app.use("/api/posts", require("./routes/api/posts"));
